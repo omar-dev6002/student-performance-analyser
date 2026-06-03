5b57b4da8b9094a2f3c20e5f6d9b7ece4c91e260
Omar Farooq <omarfarooqanis@outlook.com>
2026-06-03 13:22:13 +0530

    Create main.py

---
+++ src/main.py
@@ -0,0 +1,18 @@
+# Student Performance Analyser
+# Author: Omar | JMI CSE (AI & Data Science)
+# Project - June 2026
+
+import pandas as pd
+import numpy as np
+
+# TODO: Load dataset
+# TODO: Exploratory Data Analysis (EDA)
+# TODO: Feature Engineering
+# TODO: Train ML Model
+# TODO: Evaluate Model
+
+def main():
+    print("Student Performance Analyser - Initializing...")
+
+if __name__ == "__main__":
+    main()
